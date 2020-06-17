cfd349bdaf2e7478a7d0f3d64df09c0a7ed35ccf
Kiri Um <kirirotha@gmail.com>
2020-06-17 21:14:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,10 @@
 
 def using_concat(array1, array2)
-  counter = 0 
+
   array_concat = array1
-  while counter < array2.length do
-    array_concat.push(array2[counter])
+
+    array_concat.push(array2)
     counter +=
-  end
   return array_concat
 end
   
